a1989597664474bb977fb9d31dbd3528feedae39
Joseph Segbefia <elorma.segbefia@gmail.com>
2023-11-03 12:43:02 +0000

    Create table and seed data into it

---
+++ app/models/restaurant.rb
@@ -0,0 +1,20 @@
+class Restaurant < ActiveRecord::Base
+  # #Create
+  # restaurant = Restaurant.new(name: "Papito de la Casa")
+  # restaurant.save
+
+
+  # # Read - Read all or one
+  # restaurants = Restaurant.all
+
+  # #read 1
+  # restaurant.find(1)
+
+  # # Update
+  # restaurant = Restaurant.find(1)
+  # restaurant.rating = 5
+  # restaurant.save
+
+  # restaurant = Restaurant.find(2)
+  # restaurant.destroy
+end
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,1 +1,23 @@
 # This is where you can create initial data for your app.
+# require 'faker'
+
+# puts "Creating restaurants"
+# 100.times do |i|
+#   restaurant = Restaurant.new(name: Faker::Restaurant.name, address: Faker::Address.street_name)
+#   restaurant.save!
+#   puts "created #{i} restaurant "
+# end
+
+# puts "All done"
+
+
+require 'json'
+require "rest-client"
+
+response = RestClient.get "https://api.github.com/users/lewagon/repos"
+repos = JSON.parse(response)
+
+repos.each do |repo|
+  restaurant = Restaurant.new(name: repo['name'])
+  restaurant.save
+end
